15716af1211636cbe0bdeebcdc7bb6f4a4b3f59f
MattC <1945493+clem81@users.noreply.github.com>
2022-04-29 13:40:02 +0100

    static methods

---
+++ Inheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿using Inheritance;
+
+Console.WriteLine("Inheritance");
+
+Chef chef = new Chef();
+chef.MakeChicken();
+
+ItalianChef italianChef = new ItalianChef();
+italianChef.MakeChicken();
+italianChef.MakePasta();
+
+chef.MakeSpecialDish();
+italianChef.MakeSpecialDish();
---
+++ Static_Methods_and_Classes/Program.cs
@@ -0,0 +1,5 @@
+﻿using Static_Methods_and_Classes;
+
+Console.WriteLine("Static Methods and Classes");
+
+UsefulTools.SayHi("Fred");
